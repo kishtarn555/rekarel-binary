69a7fc5c8086c0fcf5e19e9bec7fa4f6f4b842f0
kishtarn555 <hec_montemoon@hotmail.com>
2024-12-26 15:06:32 -0600

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { dts } from "rollup-plugin-dts";
+
+const ts = typescript();
+
+export default [{
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+      name:"karel"
+    },
+    
+    plugins: [
+        commonjs(),
+        nodeResolve(),
+        ts
+    ]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.js',
+      format: 'es',
+      name:"karel"
+    },
+    
+    plugins: [
+        commonjs(),
+        nodeResolve(),
+        ts,
+    ]
+  },
+  {
+    // path to your declaration files root
+    input: './dist/built/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'cjs' }],
+    plugins: [dts()],
+  }
+
+];
