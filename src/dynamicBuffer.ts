de107eef58318bae27e188c8c058e547c177ce31
kishtarn555 <hec_montemoon@hotmail.com>
2024-12-26 18:45:47 -0600

    [Feature] Add optional offset to write in DynamicBuffer

--- src/dynamicBuffer.ts
+++ src/dynamicBuffer.ts
@@ -19,52 +19,78 @@ export class DynamicBuffer {
         this.view = new DataView(this.buffer);
     }
 
-    private ensureCapacity(size: number) {
-        if (this.offset + size > this.buffer.byteLength) {
-            const newSize = Math.max(this.buffer.byteLength * 2, this.offset + size);
-            this.buffer = resizeBuffer(this.buffer, newSize);
+    private ensureCapacity(size: number, writeOffset: number) {
+        const requiredSize = writeOffset + size;
+        if (requiredSize > this.buffer.byteLength) {
+            const newSize = Math.max(this.buffer.byteLength * 2, requiredSize);
+            const newBuffer = new ArrayBuffer(newSize);
+            new Uint8Array(newBuffer).set(new Uint8Array(this.buffer)); // Copy old data
+            this.buffer = newBuffer;
             this.view = new DataView(this.buffer);
         }
     }
 
-    writeInt8(value: number) {
-        this.ensureCapacity(1);
-        this.view.setInt8(this.offset, value);
-        this.offset += 1;
+    writeInt8(value: number, offset?: number) {
+        const writeOffset = offset !== undefined ? offset : this.offset;
+        this.ensureCapacity(1, writeOffset);
+        this.view.setInt8(writeOffset, value);
+        if (offset === undefined) this.offset = Math.max(this.offset, writeOffset+1);
     }
 
-    writeUint8(value:number) {
-        this.ensureCapacity(1);       
-        this.view.setUint8(this.offset, value);
-        this.offset += 1;
+    writeUint8(value: number, offset?: number) {
+        const writeOffset = offset !== undefined ? offset : this.offset;
+        this.ensureCapacity(1, writeOffset);
+        this.view.setUint8(writeOffset, value);
+        if (offset === undefined) this.offset = Math.max(this.offset, writeOffset+1);
     }
 
-    writeUint16(value:number) {
-        this.ensureCapacity(2);       
-        this.view.setUint16(this.offset, value);
-        this.offset += 2;
+    writeUint16(value: number, offset?: number) {
+        const writeOffset = offset !== undefined ? offset : this.offset;
+        this.ensureCapacity(2, writeOffset);
+        this.view.setUint16(writeOffset, value);
+        if (offset === undefined) this.offset = Math.max(this.offset, writeOffset+2);
     }
 
-    writeUint32(value:number) {
-        this.ensureCapacity(4);       
-        this.view.setUint32(this.offset, value);
-        this.offset += 4;
+    writeUint32(value: number, offset?: number) {
+        const writeOffset = offset !== undefined ? offset : this.offset;
+        this.ensureCapacity(4, writeOffset);
+        this.view.setUint16(writeOffset, value);
+        if (offset === undefined) this.offset = Math.max(this.offset, writeOffset+4);
     }
 
-    writeChar(value: string) {
+    writeChar(value: string, offset?: number) {
         if (value.length !== 1) {
             throw new Error("writeChar expects a single character.");
         }
-        this.ensureCapacity(1);
-        this.view.setUint8(this.offset, value.charCodeAt(0));
-        this.offset += 1;
+        const writeOffset = offset !== undefined ? offset : this.offset;
+        this.ensureCapacity(1, writeOffset);
+        this.view.setUint8(writeOffset, value.charCodeAt(0));
+        if (offset === undefined) this.offset = Math.max(this.offset, writeOffset+1);
     }
 
-    writeString(value: string) {
+    writeString(value: string, offset?: number) {
+        const writeOffset = offset !== undefined ? offset : this.offset;
         for (let i = 0; i < value.length; i++) {
-            this.writeChar(value[i]);
+            this.writeChar(value[i], writeOffset + i);
         }
-        this.writeChar('\0'); // Null-terminate the string
+        this.writeChar('\0', writeOffset + value.length); // Null-terminate the string
+        if (offset === undefined) this.offset = Math.max(this.offset, writeOffset+value.length+1);;
+    }
+
+    /**
+     * Gets the size of the written data
+     * @returns The size of the data
+     */
+    getSize() {
+        return this.offset;
+    }
+
+    /**
+     * Returns the size of the buffer, including unwritten memory
+     * @returns the size of the buffer.
+     */
+    getCapacity() {
+        return this.buffer.byteLength;
     }
 
     getBuffer(): ArrayBuffer {
